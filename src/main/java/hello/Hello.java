1daa6d8bba889e2462ca16ccc9c8ec46de1e784a
opheliah95 <opheliah95@gmail.com>
2022-04-27 16:40:22 +0100

    added test

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,8 +1,13 @@
 package hello;
+import java.time.LocalDate;
 
 public class Hello {
     public static void main(String[] args) {
       Greeter greeter = new Greeter();
+      LocalDate date = LocalDate.now();
+      System.out.println("Date: - " + date);
+      System.out.println("The month is: "+ date.getMonth());
+      System.out.println("The day of month is : - " + date.getDayOfMonth());
       System.out.println(greeter.sayHello());
     }
   }
